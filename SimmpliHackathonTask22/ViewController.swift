c5d1c63d13a0a43309823ee6adecaafade9a854e
Apple <khalid.moin@w3villa.com>
2022-09-24 10:53:38 +0530

    firstCommit

---
+++ SimmpliHackathonTask22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimmpliHackathonTask22
+//
+//  Created by Apple on 24/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
